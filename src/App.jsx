bd273381d2df1fdb970dd9c291a2db14b30bb0f5
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2024-01-23 10:21:42 -0300

    Implementado o componente Barra de Pesquisa

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import './App.css'
+
+import BarraDePesquisa from './components/BarraDePesquisa'
 import Sidebar from './components/Sidebar'
 
 function App() {
 
   return (
-    <>
+    <div className='container'>
       <Sidebar />
-    </>
+      <BarraDePesquisa />
+    </div>
   )
 }
 
---
+++ src/components/BarraDePesquisa/index.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import './styles.css';
+
+export default function BarraDePesquisa() {
+  const [termoPesquisa, setTermoPesquisa] = useState('')
+  console.log(termoPesquisa)
+
+  return(
+  <input type="search" className='barra-pesquisa' placeholder="Digite o que você procura" value={termoPesquisa} onChange={event => setTermoPesquisa(event.target.value)} />
+  )
+}
+
